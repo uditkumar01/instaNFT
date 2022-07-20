4b49d79fae4b124bfbf9e7486a70373cea8cbf0d
Udit Kumar <uditkumarussa@gmail.com>
2022-07-20 19:45:18 +0530

    removed unused packages

--- src/pages/api/imageProxy.ts
+++
@@ -1,7 +0,0 @@
-import { withImageProxy } from "@blazity/next-image-proxy";
-
-export default withImageProxy({
-  // whitelisting all https:// images
-  whitelistedPatterns: [/^https:\/\//],
-  fallbackUrl: "/images/broken.png",
-});
